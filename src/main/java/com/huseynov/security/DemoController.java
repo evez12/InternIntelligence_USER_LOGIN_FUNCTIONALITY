6ed672ca7c2ec435b78f231f839e856e36401d86
evez12 <156583224+evez12@users.noreply.github.com>
2024-12-24 17:13:52 +0400

    create demo controller

--- src/main/java/com/huseynov/security/DemoController.java
+++ src/main/java/com/huseynov/security/DemoController.java
@@ -1,4 +1,15 @@
 package com.huseynov.security;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
 public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HELLO EVERYBODY";
+    }
 }
