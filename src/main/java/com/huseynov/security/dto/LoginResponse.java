9178310c02e66c39fd2ca20a0495714badf351c8
evez12 <156583224+evez12@users.noreply.github.com>
2024-12-27 21:57:53 +0400

    added: LoginResponse(Request)

---
+++ src/main/java/com/huseynov/security/dto/LoginResponse.java
@@ -0,0 +1,16 @@
+package com.huseynov.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String username;
+    private List<String> roles;
+}
